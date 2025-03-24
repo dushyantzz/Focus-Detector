2d6587cca7ae2cc907afd1e6d9ed6b74fc7881ba
Dushyant <157586156+dushyantzz@users.noreply.github.com>
2025-03-25 00:51:13 +0530

    Update main1.py

--- focus detector/main1.py
+++ focus detector/main1.py
@@ -4,5 +4,4 @@ def download_shape_predictor():
     dlib.download_model("shape_predictor_68_face_landmarks.dat.bz2")
     print("Shape predictor downloaded successfully.")
 
-# Call this function before initializing GazeMonitor
 download_shape_predictor()
